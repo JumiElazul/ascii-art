d59227b8dcd39722ffd968f4d8b097939e5a40a1
Gregory Coomber <gr.coomber@gmail.com>
2025-01-04 10:16:07 -0800

    Changed the @ and $ symbol for lightness value in the complex ascii ramp

--- src/ascii_parser.cpp
+++ src/ascii_parser.cpp
@@ -12,7 +12,7 @@
 
 ascii_parser::ascii_parser(const image& img, const parsed_args& args, std::fstream* out_file)
     : _ascii_ramp_simple(" .:-=+*#%@")
-    , _ascii_ramp_complex(" .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$")
+    , _ascii_ramp_complex(" .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B$@")
     , _args(args)
 {
 #ifdef DEBUG
